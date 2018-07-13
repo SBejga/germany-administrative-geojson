4aa057c8a3b054cd168ea7b9dabea69b894400da
Sebastian Bejga <sebastian@bejga.de>
2018-07-13 15:42:32 +0200

    fix test scope (ignore build)

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
         "^.+\\.tsx?$": "ts-jest",
     },
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testPathIgnorePatterns: ["/node_modules/", "/build/"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
